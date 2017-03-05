63f06be048e5c81e53cd869f3de398b5c2a413c9
YuRuei <pupuandtata@gmail.com>
2017-03-05 13:25:59 +0800

    2017030501

---
+++ Myjava/src/tw/org/iii/Brad01.java
@@ -0,0 +1,14 @@
+package tw.org.iii;
+
+public class Brad01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		//abcdeg
+		
+		//XXX ????
+	}
+
+
+}
---
+++ Myjava/src/tw/org/iii/Hello.java
@@ -0,0 +1,8 @@
+package tw.org.iii;
+
+public class Hello {
+    public static void main(String[]  julie){
+        System.out.println("Hello,World");
+        System.out.println("Hello,Brad");
+    }
+}
