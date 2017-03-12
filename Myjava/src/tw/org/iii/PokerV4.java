f8b398c2d542beabcdfb19dbd6e1f92954976c71
YuRuei <pupuandtata@gmail.com>
2017-03-12 11:48:17 +0800

    1060312-2

--- Myjava/src/tw/org/iii/PokerV4.java
+++ Myjava/src/tw/org/iii/PokerV4.java
@@ -4,6 +4,7 @@ public class PokerV4 {
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
+		//洗牌
 		boolean[] check = new boolean[52];  // 0-51:false
 		int[] poker = new int [52];   // 0-51:0
 		int temp; int counter=0;
@@ -19,11 +20,34 @@ public class PokerV4 {
 			System.out.println(temp);
 			
 		}
-	
 			
 		System.out.println("號時:"+(System.currentTimeMillis() - start));  //字串沒有減的運算
 		System.out.println("次數:"+counter);
 		
+		
+		//發牌 = >四個玩家 => 每家有13張
+		int[][] player = new int[4][13];
+		for(int i=0; i<poker.length; i++){
+			player[i%4][i/4] = poker[i];
+		}
+		System.out.println("---------------------------");
+		//驗算第一家
+		for (int v :player[0]){
+			System.out.println(v);
+		}
+		System.out.println("---------------------------");
+		//驗算第二家
+		for (int v :player[1]){
+			System.out.println(v);
+		//攤牌
+		for(int[] cards:player){
+			for(int card : cards){
+				System.out.print(card + "  ");
+			}
+			System.out.println();
+		}
+		}
+	
 
 	}
 
---
+++ Myjava/src/tw/org/iii/PokerV5.java
@@ -0,0 +1,46 @@
+package tw.org.iii;
+
+import java.util.Arrays;
+
+public class PokerV5 {
+
+	public static void main(String[] args) {
+		// 洗牌
+				boolean[] check = new boolean[52];	// 0 - 51: false
+				int[] poker = new int[52]; // 0- 51: 0
+				int temp; int counter=0;
+				for (int i=0; i<52; i++){
+					do{
+						temp = (int)(Math.random()*52);
+						counter++;
+					}while (check[temp]);
+					
+					poker[i] = temp;
+					check[temp] = true;
+					System.out.println(temp);
+				}
+				// 發牌 => 四個玩家 => 每家有13張
+				int[][] player = new int[4][13];
+				for (int i=0; i<poker.length; i++){
+					player[i%4][i/4] = poker[i];
+				}
+				System.out.println("--");
+				// 攤牌
+				String[] suits = {"黑桃","紅心","方塊","梅花"}; // suits[0]=黑桃
+				String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}; // values[0]=A
+				
+				for (int[] cards : player){
+					//理牌
+					Arrays.sort(cards);
+					for (int card : cards){
+						System.out.print(suits[card/13] + values[card%13] + "  ");
+						
+					}
+					System.out.println();
+				}
+				
+				
+
+	}
+
+}
